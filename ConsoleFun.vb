7d2f0a510a284ba9492dc9bd532f39aae6b4c7f8
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-08-24 08:38:41 -0600

    add convert example

--- ConsoleFun.vb
+++ ConsoleFun.vb
@@ -1,14 +1,26 @@
-﻿Module ConsoleFun
+﻿Option Strict On
+Option Explicit On
+
+
+
+Module ConsoleFun
 
     Sub Main()
         Dim userInput As String
-        Console.WriteLine("Please Type Some Text Then Press Enter...")
+        'Console.WriteLine("Please Type Some Text Then Press Enter...")
+        'userInput = Console.ReadLine()
+        'Console.WriteLine("Thanks!! You entered: " & userInput & ". Good Work!!")
+        'Console.ReadLine()
+
+        Dim firstNumber As Integer
+        Dim result As Integer
+        Console.WriteLine("Please Enter a Number")
         userInput = Console.ReadLine()
-        Console.WriteLine("Thanks!! You entered: " & userInput & ". Good Work!!")
+        firstNumber = CInt(userInput)
+        'firstNumber = Console.ReadLine()
+        result = 5 + firstNumber
+        Console.WriteLine(result)
         Console.ReadLine()
-
-
-
 
 
     End Sub
