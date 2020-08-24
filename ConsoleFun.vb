6ee2bb68f149b7b4f9e19ddb86e73954d0e032cb
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-08-24 08:21:38 -0600

    Add project files.

---
+++ ConsoleFun.vb
@@ -0,0 +1,16 @@
+﻿Module ConsoleFun
+
+    Sub Main()
+        Dim userInput As String
+        Console.WriteLine("Please Type Some Text Then Press Enter...")
+        userInput = Console.ReadLine()
+        Console.WriteLine("Thanks!! You entered: " & userInput & ". Good Work!!")
+        Console.ReadLine()
+
+
+
+
+
+    End Sub
+
+End Module
